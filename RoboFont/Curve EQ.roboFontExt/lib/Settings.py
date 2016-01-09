7a0c563d52382c83bd888642e5c7d793178e8a9e
Jens Kutilek <webmail@kutilek.de>
2016-01-09 14:07:41 +0100

    Update Settings

--- RoboFont/Curve EQ.roboFontExt/lib/Settings.py
+++ RoboFont/Curve EQ.roboFontExt/lib/Settings.py
@@ -1,5 +1,11 @@
 from Analytics import Analytics
 from EQExtensionID import extensionID
+
+# Make jkRFExtensionSettings importable
+from os.path import dirname, join
+from sys.path import append
+append(join(dirname(__file__), "externals", "jkRFExtensionSettings", "lib"))
+
 from jkRFExtensionSettings.SettingsWindow import SettingsWindow
 
 my_settings = SettingsWindow(extensionID, "Curve Equalizer Settings", True, Analytics())
@@ -10,7 +16,7 @@ my_settings.width = 276
 my_settings.add("previewCurves", False, "Always show curve preview")
 my_settings.add("previewHandles", False, "Always show handle preview")
 my_settings.add("drawGeometry", False, "Draw triangle geometry")
-my_settings.add("stats", True, "Send debugging and usage statistics")
+#my_settings.add("stats", True, "Send debugging and usage statistics")
 my_settings.add("debug", False, "Run in debug mode")
 
 my_settings.show()
