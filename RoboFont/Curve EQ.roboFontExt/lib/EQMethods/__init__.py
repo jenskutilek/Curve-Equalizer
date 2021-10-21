a7fca8da97ad63b562fc9f03662dd238c1e81fe0
Jens Kutilek <webmail@kutilek.de>
2021-10-21 09:38:28 +0200

    Remove "rule of thirds" code

--- RoboFont/Curve EQ.roboFontExt/lib/EQMethods/__init__.py
+++ RoboFont/Curve EQ.roboFontExt/lib/EQMethods/__init__.py
@@ -2,13 +2,11 @@ from __future__ import absolute_import
 from .Balance import eqBalance
 from .HobbySpline import eqSpline
 from .Percentage import eqPercentage
-#from .Quadratic import eqQuadratic
-from .RuleOfThirds import eqThirds
+# from .Quadratic import eqQuadratic
 
 __all__ = [
     "eqBalance",
     "eqPercentage",
     # "eqQuadratic",
     "eqSpline",
-    "eqThirds",
 ]
