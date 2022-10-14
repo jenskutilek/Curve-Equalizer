b4a93482f7a10c2faeb7257f46e3485106917f6a
Jens Kutilek <webmail@kutilek.de>
2022-10-14 14:22:09 +0200

    Revert "Remove "rule of thirds" code"
    This reverts commit a7fca8da97ad63b562fc9f03662dd238c1e81fe0.

--- RoboFont/Curve EQ.roboFontExt/lib/EQMethods/__init__.py
+++ RoboFont/Curve EQ.roboFontExt/lib/EQMethods/__init__.py
@@ -3,11 +3,13 @@ from __future__ import annotations
 from .Balance import eqBalance
 from .HobbySpline import eqSpline
 from .Percentage import eqPercentage
-# from .Quadratic import eqQuadratic
+#from .Quadratic import eqQuadratic
+from .RuleOfThirds import eqThirds
 
 __all__ = [
     "eqBalance",
     "eqPercentage",
     # "eqQuadratic",
     "eqSpline",
+    "eqThirds",
 ]
